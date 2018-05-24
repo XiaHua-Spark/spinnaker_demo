6fcd2b1ce82b0c0523acd543319a7fa99df81eed
e <xhua.spark@gmail.com>
2018-05-24 22:09:29 +0800

    add some discription

--- spinnaker_demo/spinnaker_demo.cpp
+++ spinnaker_demo/spinnaker_demo.cpp
@@ -13,6 +13,8 @@ using namespace Spinnaker::GenApi;
 using namespace Spinnaker::GenICam;
 using namespace std;
 
+
+// convert captured image and return it as a opencv Mat 
 cv::Mat AcquireSingleImage(CameraPtr pCam)
 {
 	pCam->BeginAcquisition();
@@ -41,6 +43,85 @@ cv::Mat AcquireSingleImage(CameraPtr pCam)
 	return cv::Mat(colsize + YPadding, rowsize + XPadding, CV_8UC1, convertedImage->GetData(), convertedImage->GetStride()).clone();
 }
 
+
+// return the coordinate of the largest contour caputured from a input camera
+cv::Point2f GetSpotCenter(CameraPtr pCam)
+{
+	pCam->BeginAcquisition();
+
+	ImagePtr pResultImage = pCam->GetNextImage();
+
+	// Convert image to mono 8
+	ImagePtr convertedImage = pResultImage->Convert(PixelFormat_Mono8, HQ_LINEAR);
+	// release camera buffer
+	pResultImage->Release();
+
+	// End acquisition
+	pCam->EndAcquisition();
+	unsigned int XPadding = convertedImage->GetXPadding();
+	unsigned int YPadding = convertedImage->GetYPadding();
+	unsigned int rowsize = convertedImage->GetWidth();
+	unsigned int colsize = convertedImage->GetHeight();
+
+	cv::Mat image(colsize + YPadding, rowsize + XPadding, CV_8UC1, convertedImage->GetData(), convertedImage->GetStride());
+
+	// blur it slightly
+	//cv::Mat blurred;
+	//cv::GaussianBlur(image, blurred, cv::Size(3, 3), 0);
+
+	// threshold it
+	cv::Mat thresh;
+	cv::threshold(image, thresh, 128, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
+
+	// find contours in the thresholded image
+	cv::Mat dst = cv::Mat::zeros(image.rows, image.cols, CV_8UC3);
+
+	vector< vector<cv::Point> > contours;
+	vector<cv::Vec4i> hierarchy;
+
+	cv::findContours(thresh, contours, hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+	// find the largest contour and its index
+	int largest_area = 0;
+	int largest_contour_index = 0;
+	cv::Rect bounding_rect;
+
+	for (size_t i = 0; i < contours.size(); i++)  // iterate through each contour.
+	{
+		double area = contourArea(contours[i]);  //  Find the area of contour
+
+		if (area > largest_area)
+		{
+			largest_area = area;
+			largest_contour_index = i;           //Store the index of largest contour
+			bounding_rect = boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
+		}
+	}
+
+
+	// Get the moments
+	cv::Moments mu;
+	mu = moments(contours[largest_contour_index], false);
+
+	// Get the mass centers:
+	cv::Point2f mc;
+	mc = cv::Point2f(mu.m10 / mu.m00, mu.m01 / mu.m00);
+
+	// generate a RGB image for display color contours
+	cv::Mat img_display;
+	cv::cvtColor(image, img_display, CV_GRAY2RGB);
+
+	// draw the largest contour
+	cv::drawContours(img_display, contours, largest_contour_index, cv::Scalar(0, 0, 255), CV_FILLED, 8, hierarchy);
+	// draw a rectangle on largest object
+	rectangle(img_display, bounding_rect, cv::Scalar(0, 255, 0), 1, 8, 0);
+	
+	cv::imshow("contour", img_display);
+	cv::waitKey();
+
+	return mc;
+}
+
 int main()
 {
 	// Print application build information
@@ -63,13 +144,15 @@ int main()
 
 	cv::Mat img= AcquireSingleImage(pCam);
 
-	pCam->DeInit();
-
 	// display captured image
-	cv::namedWindow("image");
-	cv::imshow("image", img);
-	cv::waitKey();
+	//cv::namedWindow("image");
+	//cv::imshow("image", img);
+	//cv::waitKey();
 
+	cv::Point2f currentMassCenter = GetSpotCenter(pCam);
+	cout << currentMassCenter << endl;
+
+	pCam->DeInit();
 	pCam = NULL;
 
 	// Clear camera list before releasing system
